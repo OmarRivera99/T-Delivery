fd0f41a161fd055587123e25102243a99692199d
OmarRivera99 <omar090699@hotmail.com>
2024-01-17 09:30:12 -0700

    Added PDFbox Added PDFCreator

--- app/src/main/java/com/example/t_delivery/Views/ui/theme/Main_View.kt
+++ app/src/main/java/com/example/t_delivery/Views/ui/theme/Main_View.kt
@@ -1,5 +1,6 @@
 package com.example.t_delivery.Views.ui.theme
-
+//https://github.com/tejpratap46/PDFCreatorAndroid/tree/master
+//https://www.geeksforgeeks.org/generate-pdf-file-in-android-using-jetpack-compose/
 class Main_View {
 
 }
